d1aa587f607641229be0fb41cb265d3c38361f8c
Fernando Tavares <fernando.adriano.tavares@gmail.com>
2023-03-10 14:56:42 -0300

    feat: criada interacoes com o bot

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 
+// dotenv
 const dotenv = require("dotenv");
 dotenv.config();
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const { TOKEN } = process.env;
 
 // importação dos comandos
-
 const fs = require("node:fs");
 const path = require("node:path");
-
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
     .readdirSync(commandsPath)
     .filter((file) => file.endsWith(".js"));
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.command = new Collection();
+client.commands = new Collection();
 
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     if ("data" in command && "execute" in command) {
-        client.command.set(command.data.name, command);
+        client.commands.set(command.data.name, command);
     } else {
         console.log(
-            `Esse comando em ${filePath} está com "data" ou "execute" ausentes`
+            `Esse comando em ${filePath} está com "data" ou "execute ausentes"`
         );
     }
 }
 
-// Login bot
+// Login do bot
 client.once(Events.ClientReady, (c) => {
     console.log(`Pronto! Login realizado como ${c.user.tag}`);
 });
-
 client.login(TOKEN);
 
-client.on(Events.InteractionCreate, (interaction) => {
+// Listener de interações com o bot
+client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
-    console.log(interaction);
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error("Comando não encontrado");
+        return;
+    }
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply("Houve um erro ao executar esse comando!");
+    }
 });
